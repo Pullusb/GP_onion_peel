669575e6968cde45696643aea08e8023edb57eb5
Pullusb <bernou.samuel@gmail.com>
2024-11-18 19:06:32 +0100

    up version compatibility on init

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ bl_info = {
     "name": "GP Onion Peel",
     "description": "Custom Onion skinning using refreshed GP duplications",
     "author": "Samuel Bernou",
-    "version": (1, 0, 1),
-    "blender": (4, 0, 0),
+    "version": (2, 0, 0),
+    "blender": (4, 3, 0),
     "location": "View3D",
     "doc_url": "https://pullusb.gumroad.com/l/gp_onion_peel",
     "category": "Object" }
